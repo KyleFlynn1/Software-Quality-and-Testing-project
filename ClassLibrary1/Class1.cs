78a7da950e2522c07fa94c8b1b301fd27ac04a73
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2025-04-24 19:07:22 +0100

    added file with needed code

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        class InsuranceService
+        {
+            public double CalcPremium(int age, string gameMode)
+            {
+                double premium;
+
+                if (gameMode == "casual")
+                    if ((age >= 18) && (age < 30))
+                        premium = 5.0;
+                    else
+                        if (age >= 31)
+                        premium = 2.50;
+                    else
+                        premium = 0.0;
+                else if (gameMode == "hardcore")
+                    if ((age >= 18) && (age <= 35))
+                        premium = 6.0;
+                    else if (age >= 36)
+                        premium = 5.0;
+                    else
+                        premium = 0.0;
+                else
+                    premium = 0.0;
+
+                DiscountService ds = new DiscountService();  // tightly coupled
+                double discount = ds.GetDiscount();
+                if (age >= 50)
+                    premium = premium * discount;
+
+                return premium;
+            }
+        }
+    }
+}
